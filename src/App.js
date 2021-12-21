1bda06f6ebcf990c75571a7ee1ce8e93abf17963
Ivan Olmo <ivan.olmo@gmail.com>
2021-12-20 22:19:15 -0600

    Update main component app feedback delete handler.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+
 function App() {
-  return <h1>My app</h1>;
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const handleDelete = (id) => {
+    if (window.confirm('Are you sure?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackList
+          feedback={feedback}
+          handleDelete={(id) => handleDelete(id)}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;

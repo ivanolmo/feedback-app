0e655213367afa9892943a3b4cf2f80a99d750f1
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-01 21:11:24 -0600

    Add card animation using framer-motion library.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
@@ -5,19 +6,43 @@ function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback </p>;
   }
+
   return (
     <div className='feedback-list'>
-      {feedback.map((item, idx) => (
-        <FeedbackItem
-          key={idx}
-          id={item.id}
-          rating={item.rating}
-          text={item.text}
-          handleDelete={handleDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item, idx) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={idx}
+              id={item.id}
+              rating={item.rating}
+              text={item.text}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item, idx) => (
+  //       <FeedbackItem
+  //         key={idx}
+  //         id={item.id}
+  //         rating={item.rating}
+  //         text={item.text}
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {

51a83e61c0554b3c26d6432965070927615c1546
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-01 21:12:32 -0600

    Add framer-motion and react-router-dom to project. Add About page to main app.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
 import Header from './components/Header';
@@ -6,13 +7,13 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './components/pages/AboutPage';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
-    console.log(newFeedback);
     setFeedback([newFeedback, ...feedback]);
   };
 
@@ -23,17 +24,20 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
-      <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList
-          feedback={feedback}
-          handleDelete={(id) => handleDelete(id)}
-        />
-      </div>
-    </>
+      <Route exact path='/'>
+        <div className='container'>
+          <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedback={feedback} />
+          <FeedbackList
+            feedback={feedback}
+            handleDelete={(id) => handleDelete(id)}
+          />
+        </div>
+      </Route>
+      <Route path='/about' component={AboutPage} />
+    </Router>
   );
 }
 

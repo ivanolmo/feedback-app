fffeeee33321b44e23b1d9a690ab3296abb8f5ce
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-03 18:44:25 -0600

    Move functions from main App file into context.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,6 @@
 import { createContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext();
@@ -6,10 +8,23 @@ const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        deleteFeedback,
+        addFeedback,
       }}
     >
       {children}

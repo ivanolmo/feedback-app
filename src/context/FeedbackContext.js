4c076c72ecefc032d6d5c80c64057a0a2ef1cc29
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-05 16:34:19 -0600

    Add json-server to project with mock db.json file.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,16 +1,28 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-
-import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackToEdit, setFeedbackToEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      'http://localhost:5000/feedback?_sort=id&_order=desc'
+    );
+
+    const data = await response.json();
+
+    setFeedback(data);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure?')) {
